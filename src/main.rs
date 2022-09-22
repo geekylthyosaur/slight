60ff7091d103322b702f7e152a4335a1f1a48d20
geekylthyosaur <geekylthyosaur@gmail.com>
2022-09-22 23:22:48 +0300

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use slight::Slight;
 
 use clap::Parser;
 
+const TIME_BETWEEN_STEPS: std::time::Duration = std::time::Duration::from_millis(64);
+
 /// An application to control backlight brightness
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -30,6 +32,6 @@ fn main() {
 
     for i in slight.range() {
         slight.set_value(i).unwrap();
-        std::thread::sleep(std::time::Duration::from_millis(64));
+        std::thread::sleep(TIME_BETWEEN_STEPS);
     }
 }

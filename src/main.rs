bb6be72e777a80db0606efe7d14b086ec2a87508
geekylthyosaur <geekylthyosaur@gmail.com>
2022-10-21 23:04:06 +0300

    Add pretend flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod error;
 mod io;
 mod slight;
+mod percent;
 mod value;
 
 use std::time::Duration;
@@ -14,10 +15,18 @@ const TIME_BETWEEN_STEPS: Duration = Duration::from_millis(64);
 /// An application to control backlight brightness
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+pub struct Args {
     /// Path to file
-    #[clap(short, long)]
+    #[clap(long)]
     path: String,
+
+    /// Use exponential brightness
+    #[clap(short)]
+    exp: bool,
+
+    /// Write to stdout instead of file
+    #[clap(short, long)]
+    pretend: bool,
 
     /// Percent
     #[clap(allow_hyphen_values = true)]
@@ -33,7 +42,11 @@ fn main() {
     };
 
     for i in slight.range() {
-        slight.set_value(i).unwrap();
-        std::thread::sleep(TIME_BETWEEN_STEPS);
+        if slight.args.pretend {
+            println!("{}", i);
+        } else {
+            slight.set_value(i).unwrap();
+            std::thread::sleep(TIME_BETWEEN_STEPS);
+        }
     }
 }
